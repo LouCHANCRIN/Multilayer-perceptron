0286bfd4e0a60816766f8e1b25f789f0e38c506b
lchancri <lchancri@student.42.fr>
2018-08-11 16:35:04 +0200

    first layer attempt

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 import numpy as np
 import sys
 
@@ -11,7 +12,19 @@ Y = data[1]
 Y = np.reshape(Y, (line, 1))
 
 X = data.drop([1], axis=1).values
+print(X)
 X = np.reshape(X, (line, col))
+
+#theta = {}
+#theta['0'] = np.reshape([[0.0] * col], (col, 1))
+#theta['1'] = np.reshape([[0.0] * col], (col, 1))
+
+rand = []
+for i in range(0, col * 2):
+    rand.append(random.randint(-50, 50) * 0.000000001)
+
+rand = np.reshape(rand, (col * 2, 1))
+theta = np.reshape([[rand]], (2, col))
 
 name = []
 for key in data:
@@ -27,11 +40,24 @@ def moy(X, line):
 
 def change_nan(X, col, line, data, name):
     for c in range(0, col):
-        if (c != 1)
+        if (c != 1):
             _moy = moy(data[name[c]], line)
             for l in range(0, line):
                 if (X[l][c] != X[l][c]):
                     X[l][c] = _moy
     return (X)
 
+def neural_network(X, Y, col, line, theta):
+    print("X : ", np.shape(X))
+    print(X)
+    print("Theta : ", np.shape(theta))
+    print(theta)
+    Z = theta.dot(np.transpose(X))
+    print("Z : ", np.shape(Z))
+    print(Z)
+    A = (np.exp(Z) - np.exp(-Z)) / (np.exp(Z) + np.exp(-Z))
+    print("A : ", np.shape(A))
+    print(A)
+
 X = change_nan(X, col, line, data, name)
+neural_network(X, Y, col, line, theta)
