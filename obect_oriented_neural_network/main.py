ffedeb34af6f7a38140f627434cb5eeb84486661
lchancri <lchancri@student.42.fr>
2018-09-11 16:13:36 +0200

    theta done

--- obect_oriented_neural_network/main.py
+++ obect_oriented_neural_network/main.py
@@ -27,6 +27,4 @@ dt.Y_soft()
 drop = [1] # features to drop in for A0
 nb_drop = 1
 dt.create_A(hyp, drop, nb_drop)
-nb_neurone = [dt.col, 30, 30, 30, 2]
-par = param.parameters(hyp, nb_neurone)
-print(dt.A[0])
+par = param.parameters(hyp, [dt.col, 30, 30, 30, 2], dt)
--- obect_oriented_neural_network/parameters.py
+++ obect_oriented_neural_network/parameters.py
@@ -9,17 +9,20 @@ def random_number(col, line, size):
         rand.append(random.randint(-50, 50) * size)
     return (rand)
 
-def set_theta(hyp, n):
+def set_theta(W, B, hyp, n):
     for i in range(1, hyp.nb_layer + 1):
         rand = random_number(n[i], n[i - 1], 0.01)
         #rand = random_number(n[i], n[i - 1], 2 / line_train)
-        W[i](np.reshape([[rand]], (n[i], n[i - 1])))
+        W[i] = (np.reshape([[rand]], (n[i], n[i - 1])))
         rand = random_number(n[i], 1, 0.00)
-        B[i](np.reshape([[0.0] * n[i]], (n[i], 1)))
+        B[i] = (np.reshape([[0.0] * n[i]], (n[i], 1)))
+    return (W, B)
 
 class parameters:
-#mettre A et A_test ici
-    def __init__(self, hyp, nb_neurone):
+
+    def __init__(self, hyp, nb_neurone, data):
+        self.A = data.A
+        self.A_test = data.A_test
         self.W = [0] * (hyp.nb_layer + 1)
         self.B = [0] * (hyp.nb_layer + 1)
         self.Z = [0] * (hyp.nb_layer + 1)
@@ -29,3 +32,4 @@ class parameters:
         self.DZ = [0] * (hyp.nb_layer + 1)
         self.DB = [0] * (hyp.nb_layer + 1)
         self.cost = []
+        self.W, self.B = set_theta(self.W, self.B, hyp, nb_neurone)
